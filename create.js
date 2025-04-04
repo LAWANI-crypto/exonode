7ecce79353c5d1b62894f86d623c2f6afabcc1d5
LAWANI-crypto <abdulakanmu1@gmail.com>
2025-04-04 09:58:16 +0100

    fix(API): create a server who add a task et show the result

--- create.js
+++ create.js
@@ -1,28 +1,34 @@
-const http = require('http');
-const Tasks = [];
+const http = require("http");
+
+const tasks = [];
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/task" && req.method === "POST") {
- 
-    const newTask = {
-        id: tasks.length + 1, 
-        title,
-        completed: false, 
-    };
-    Tasks.push(newTask); 
-    res.writeHead(201,{ "content-type": "application/json"})
-    res.end(JSON(newTask))
-}
+  if (req.url === "/task" && req.method === "POST") {
+    let body = "";
+    let task;
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      task = JSON.parse(body);
+
+      tasks.push(task);
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(tasks));
+    });
+  } else if (req.url === "/tasks" && req.method === "GET") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store",
+    });
+    res.end(JSON.stringify(tasks));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: "Not Found" }));
+  }
 });
-server => {
-    if (req.url === "/tasks" && req.method === "POST") {
-        res.writeHead(200, { "content-type": "application/json", "cache-control": "no-store" });
-        res.end(JSON.stringify(Tasks))
-    }
-};
 
 const port = 3000;
 server.listen(port, () => {
-    console.log(`Serveur demarree sur http://localhost:${port}`);
+  console.log(`Serveur démarré sur http://localhost:${port}`);
 });
-
