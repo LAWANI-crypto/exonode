4c6d356568ed5e0430d39c00342dab6b6c78828c
LAWANI-crypto <abdulakanmu1@gmail.com>
2025-04-09 10:03:56 +0100

    fix(API): files modify by puting function in good place for structuration of the work

--- create.js
+++ create.js
@@ -1,46 +1,56 @@
+const { Console } = require("console");
 const http = require("http");
 
 const tasks = [];
 const STATES = ["done", "onhold", "pending", "undone"];
+let task;
+
+function recup(req) {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+  req.on("end", () => { 
+    task = JSON.parse(body);
+  })
+};
+function add(task, res){
+  const isValidObject =
+  typeof task === "object" &&
+  task !== null
+
+  const hasRequiredKeys =
+      "id" in task &&
+      "title" in task &&
+      "status" in task;
+    
+    const isValidId = typeof task.id === "number";
+    console.log(typeof task.id);
+    const isValidTitle = typeof task.title === "string";
+    console.log(isValidTitle)
+    const isValidStatus =
+      STATES.includes(task.status);
+
+  if (isValidObject && hasRequiredKeys && isValidId && isValidTitle && isValidStatus) {
+    tasks.push(task);
+    res.writeHead(201, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(tasks));
+  }else {
+    res.writeHead(400, { "Content-Type": "application/json" });
+    res.end(   JSON.stringify({ error: "please printf valid object" }))
+  }     
+
+}
 
 const server = http.createServer((req, res) => {
   if (req.url === "/tasks" && req.method === "POST") {
-    let body = "";
-    let task;
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-    req.on("end", () => { 
-     try {
-      task = JSON.parse(body);
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'JSON reçu !' }));
-    } catch (error) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ error: 'JSON invalide' }));
-    }
-      
-
-      const isValidObject =
-      typeof task === "object" &&
-      task !== null
-
-      const hasRequiredKeys =
-          "id" in task &&
-          "title" in task &&
-          "status" in task;
-
-        const isValidStatus =
-          STATES.includes(task.status);
-
-      if (isValidObject && hasRequiredKeys && isValidStatus ) {
-        tasks.push(task);
-        res.writeHead(201, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(tasks));
-      }else {
+    recup(req, (err, task) => {
+      if (err) {
         res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(   JSON.stringify({ error: "Objet invalide" }))
-      }     
+        res.end(JSON.stringify({ error: err.message }));
+      } else {
+        add(task, res);
+      }
     });
   } else if (req.url === "/tasks" && req.method === "GET") {
     res.writeHead(200, {
@@ -53,8 +63,7 @@ const server = http.createServer((req, res) => {
     res.end(JSON.stringify({ error: "Not Found" }));
   }
 });
-
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Serveur démarré sur http://localhost:${port}`);
+  console.log(`Server start on http://localhost:${port}`);
 });
